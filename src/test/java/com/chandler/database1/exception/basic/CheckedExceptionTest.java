ca3ef0c287db3c05aac367c49251f2e8df66b809
chandler <parksm0529@gmail.com>
2025-01-16 20:18:31 +0900

    Example of checkedException

--- src/test/java/com/chandler/database1/exception/basic/CheckedExceptionTest.java
+++ src/test/java/com/chandler/database1/exception/basic/CheckedExceptionTest.java
@@ -1,9 +1,26 @@
 package com.chandler.database1.exception.basic;
 
+import com.chandler.database1.repository.RepositoryV3;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Slf4j
 public class CheckedExceptionTest {
 
+    @Test
+    void check_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void check_throw(){
+        Service service = new Service();
+        assertThrows(MyCheckedException.class, () -> service.callThrow()); // 예외가 발생해야 테스트 성공인 경우 검증 방법
+    }
+    
     static class MyCheckedException extends Exception {
         public MyCheckedException(String message) {
             super(message);
@@ -11,12 +28,27 @@ public class CheckedExceptionTest {
     }
 
     static class Service {
+        Repository repository = new Repository();
+
+        //TODO: 예외 잡아서 처리
+        public void callCatch() {
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        //TODO: 예외를 밖으로 던지기
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
 
     }
 
     static class Repository {
-        public void call() {
-            throw new MyCheckedException("ex");
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("MyCheckedException ex");
         }
     }
 }
